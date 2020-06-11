5f6702095a1f77b30804f41e2cde25d0f81054bf
François Pluchino <francois.pluchino@klipper.dev>
2020-06-11 14:41:47 +0200

    Add list response

---
+++ models/responses/ListResponse.ts
@@ -0,0 +1,18 @@
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+export interface ListResponse<T> {
+    results: T[];
+    count: number;
+    total: number;
+    lastId: string | null;
+}
