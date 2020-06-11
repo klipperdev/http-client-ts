a9dc83ca0dd7ef252bbd43241f51719e23abc66f
François Pluchino <francois.pluchino@klipper.dev>
2020-06-11 14:41:41 +0200

    Add message response

---
+++ models/responses/MessageResponse.ts
@@ -0,0 +1,15 @@
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+export interface MessageResponse {
+    message: string;
+}
